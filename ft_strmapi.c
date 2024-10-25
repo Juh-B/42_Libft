a03ca20cb87cce3afec0a510d42e9092ac58dac4
Juliana Bonardi <julicbonardi@hotmail.com>
2024-10-24 23:00:25 -0300

    add ft_strmapi

---
+++ ft_strmapi.c
@@ -0,0 +1,24 @@
+#include "libft.h"
+
+// Apply the function ’f’ to each character of the string ’s’,
+// passes its index as the first argument to create a new string
+// (with malloc(3)) resulting from successive applications of ’f’.
+
+char  *ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+  unsigned int i;
+  size_t len;
+  char  *newstr;
+
+  len = ft_strlen(s);
+  newstr = (char *)malloc((len + 1) * sizeof(char));
+  if (newstr == NULL)
+    return (NULL);
+  i = 0;
+  while (s[i])
+  {
+    newstr[i] = f(i, &s[i]);
+    i++;
+  }
+  newstr[i] = '\0';
+}
