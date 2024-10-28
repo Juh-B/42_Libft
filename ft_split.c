960f9cdff67c42c79a4f4ee5de588d1c00e768c3
Juliana Bonardi <julicbonardi@hotmail.com>
2024-10-27 22:00:31 -0300

    updating split

--- ft_split.c
+++ ft_split.c
@@ -11,73 +11,105 @@
 /* ************************************************************************** */
 
 #include "libft.h"
-// #include <stdio.h>
-// #include <stdlib.h>
 
 // Allocate (with malloc(3)) and returns an array of strings
 // obtained by dividing by ’s’, using the character ’c’ as a delimiter.
 // The array must end with a NULL pointer.
 
-char	**ft_split(char const *s, char c)
+static size_t elem_array(const char *s, char c)
+{
+	size_t	i;
+	size_t	array_len;
+	int			control_elem;
+
+	 i = 0;
+	 array_len = 0;
+	 control_elem = 0;
+	while (s[i])
+	{
+		if (s[i] == c && control_elem == 0)
+		{
+			array_len++;
+			control_elem = 1;
+		}
+		else
+			control_elem = 0;
+		while (s[i] == c && s[i + 1] == c)
+			i++;
+		if (s[i] != c && s[i + 1] == '\0')
+			array_len++;
+		i++;
+	}
+	return (array_len);
+}
+
+static char *new_string(const char *str, unsigned int	i, unsigned int	start)
+{
+	size_t	str_len;
+	char  	*new_str;
+
+	str_len = i - start;
+	if (str[i + 1] == '\0')
+		str_len++;
+	new_str = (char *)malloc(str_len + 1 * sizeof(char));
+	ft_memcpy(new_str, str, str_len);
+	new_str[str_len] = '\0';
+	return (new_str);
+}
+
+static char	*allocate_array(char **array, char const *s, char c)
 {
 	unsigned int	i;
 	unsigned int	j;
-	unsigned int	len;
-	unsigned int	n_str;
-	size_t			str_len;
-	char			**array;
+	unsigned int	start;
 
 	i = 0;
 	j = 0;
-	len = 0;
-	n_str = 0;
+	start = 0;
 	while (s[i])
 	{
-		if (s[i] == c)
-			n_str++;
-		i++;
-	}
-	array = (char **)malloc((n_str + 1) * sizeof(char *));
-	if (array == NULL)
-		return (NULL);
-
-	str_len = 0;
-	i = 0;
-	while (s[i])
-	{
-		if (s[i] == c)
+		if (s[i] == c || s[i + 1] == '\0')
 		{
-			str_len = i + 1 - str_len;
-			array[j] = (char *)malloc(str_len + 1 * sizeof(char));
+			array[j] = new_string(s + start, i, start);
 			if (array[j] == NULL)
+			{
+				free(array);
 				return (NULL);
-			ft_memcpy(array[j], &s[len], str_len);
-			array[j][str_len] = '\0';
-			len = i + 1;
+			}
+			start = i + 1;
 			j++;
-		}
-		else if (s[i + 1] == '\0')
-		{
-			str_len = i + 1 - str_len;
-			array[j] = (char *)malloc(str_len + 1 * sizeof(char));
-			if (array[j] == NULL)
-				return (NULL);
-			ft_memcpy(array[j], &s[len], str_len);
-			array[j][str_len] = '\0';
-			len = i + 1;
+			while (s[i + 1] == c)
+			{
+				i++;
+				start++;
+			}
 		}
 		i++;
 	}
 	return (array);
 }
 
-// int	main(void)
-// {
-// 	char	**str;
+char	**ft_split(char const *s, char c)
+{
+	size_t			array_len;
+	char			**array;
 
-// 	str = ft_split("Olaaa Mundooo Adoravel", ' ');
-// 	// printf("%s\n", *str);
-// 	// printf("1. %s\n", str[0]);
-// 	// printf("2. %s\n", str[1]);
-// 	// free(str);
-// }
+	array_len = elem_array(s, c);
+	array = (char **)malloc((array_len + 1) * sizeof(char *));
+	if (array == NULL)
+		return (NULL);
+	array[array_len] = NULL;
+	allocate_array(array, s, c);
+	return (array);
+}
+
+int	main(void)
+{
+	char	**str;
+
+	str = ft_split("Oi Mundo Doido", ' ');
+	// printf("%s\n", *str);
+	// printf("1. %s\n", str[0]);
+	// printf("2. %s\n", str[1]);
+	free(str);
+}
