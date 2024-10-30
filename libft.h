c13ec295a262ec1a8ba8b4e692ed2ebc7e79178f
Juliana Bonardi <julicbonardi@hotmail.com>
2024-10-30 00:51:41 -0300

    add lstmap from bonus

--- libft.h
+++ libft.h
@@ -69,11 +69,14 @@ char	**ft_split(char const *s, char c);
 // Bonus
 t_list  *ft_lstnew(void *content);
 t_list  *ft_lstlast(t_list *lst);
+t_list  *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
+
 void  ft_lstadd_front(t_list **lst, t_list *new);
 void  ft_lstadd_back(t_list **lst, t_list *new);
 void  ft_lstdelone(t_list *lst, void (*del)(void *));
 void  ft_lstclear(t_list **lst, void (*del)(void*));
 void  ft_lstiter(t_list *lst, void (*f)(void *));
+
 int ft_lstsize(t_list *lst);
 
 #endif
